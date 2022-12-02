d4c3ba81f1c8b60d574abbf7b01e003a2ce851e7
Ismael Arias <ismaelariasmn@gmail.com>
2022-12-02 22:56:43 +0100

    Modify day 1 to follow same structure as day 2
    * Try to keep same structure for all the days

--- 2022/day1/calories.go
+++ 2022/day1/calories.go
@@ -1,5 +1,3 @@
-//Part one
-
 package main
 
 import (
@@ -7,11 +5,17 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"sort"
 	"strconv"
 )
 
 func main() {
-	file, err := os.Open("calories_input.txt")
+	part1()
+	part2()
+}
+
+func part1() {
+	file, err := os.Open("input.txt")
 
 	if err != nil {
 		log.Fatal(err)
@@ -23,7 +27,6 @@ func main() {
 
 	//We could use a map/slice for this but not really worth it
 	elfMaxCalories := 0
-	elfMaxPosition := 0
 	currentElfPosition := 1
 	currentElfCalories := 0
 
@@ -44,11 +47,57 @@ func main() {
 
 		if currentElfCalories > elfMaxCalories {
 			elfMaxCalories = currentElfCalories
-			elfMaxPosition = currentElfPosition
 		}
 	}
 
-	fmt.Printf("Elf number %d carrying %d calories\n", elfMaxPosition, elfMaxCalories)
+	fmt.Printf("Part 1 result: %d\n", elfMaxCalories)
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func part2() {
+	file, err := os.Open("input.txt")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	//Using slices since we don't need the elf position
+	var elfCalories []int
+	currentElfCalories := 0
+
+	for scanner.Scan() {
+		if scanner.Text() == "" {
+			elfCalories = append(elfCalories, currentElfCalories)
+			currentElfCalories = 0
+			continue
+		}
+
+		line, err := strconv.Atoi(scanner.Text())
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		currentElfCalories += line
+	}
+
+	//Sort the slice
+	sort.Sort(sort.Reverse(sort.IntSlice(elfCalories)))
+
+	//Sum the first three
+	totalThreeCalories := 0
+	for _, current := range elfCalories[0:3] {
+		totalThreeCalories += current
+	}
+
+	fmt.Printf("Part 2 result: %d\n", totalThreeCalories)
 
 	if err := scanner.Err(); err != nil {
 		log.Fatal(err)
