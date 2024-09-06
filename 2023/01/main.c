0a3d8d1a771492d5aed7192fe9bc4438386264c2
Ismael Arias <ismaelariasmn@gmail.com>
2024-09-06 19:43:44 +0200

    Finish day 1 part 1

--- 2023/01/main.c
+++ 2023/01/main.c
@@ -1,21 +1,54 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
+
 #include "../helper.h"
 
+int part_1(char **file_content) {
+    int cal_sum = 0;
+    for (size_t i = 0; file_content[i] != NULL; i++) {
+        char *line = file_content[i];
+
+        // We need to keep the first digit and the last digit.
+        // Init both to -1 to make sure that we know how to continue.
+        int first_digit = -1;
+        int last_digit = -1;
+        for (size_t j = 0; line[j] != '\0'; j++) {
+            if (isdigit(line[j])) {
+                // To convert the characters to digit, we need to understand how ASCII encoding works.
+                // ASCII assigns an unique integer value to each char.
+                // We need to know that the ASCII value of '0' is 48, '1' is 49... and so on.
+                // Knowing this, the only thing we need to do, since all the digits are followed by each other,
+                // is substract '0' to each digit that we find, to finally get the integer that we want.
+                if (first_digit == -1) {
+                    first_digit = line[j] - '0';
+                }
+                // Keep updating until we run out of lines.
+                last_digit = line[j] - '0';
+            }
+        }
+        // If only one digit, the digit repeats.
+        if (last_digit == -1) {
+            last_digit = first_digit;
+        }
+        // Construct the number and sum to total calibration.
+        cal_sum += first_digit * 10 + last_digit;
+    }
+
+    return cal_sum;
+}
+
 int main() {
-    FILE *fptr = fopen("inputa.txt", "r");
+    FILE *fptr = fopen("input.txt", "r");
     char **file_content = read_file(fptr);
 
     if (file_content == NULL) {
         return 1;
     }
 
-    for (size_t i = 0; file_content[i] != NULL; i++) {
-        printf("%s", file_content[i]);
-        free(file_content[i]);  // Free each line
-    }
-    free(file_content);
+    printf("Part 1: %d\n", part_1(file_content));
 
     return 0;
 }
+
--- 2023/helper.c
+++ 2023/helper.c
@@ -1,57 +1,57 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define MORE_LINES 1024  // Define a constant for the initial number of lines to allocate
-#define MORE_CHARS 128   // Define a constant for the initial number of characters per line
+#define MORE_LINES 1024  // Define a constant for the initial number of lines to allocate.
+#define MORE_CHARS 128   // Define a constant for the initial number of characters per line.
 
-// Helper function to read the entire content of a file
+// Helper function to read the entire content of a file.
 char **read_file(FILE *fptr) {
-    // Check if the file pointer is NULL, meaning the file couldn't be opened
+    // Check if the file pointer is NULL, meaning the file couldn't be opened.
     if (fptr == NULL) {
         printf("File does not exist\n");
         return NULL;
     }
 
-    // Allocate initial memory for an array of strings (lines)
+    // Allocate initial memory for an array of strings (lines).
     char **lines = malloc(sizeof(char *) * MORE_LINES);
-    size_t total_lines = 0;  // Counter for the total number of lines read
-    size_t total_chars = 0;  // Counter for the number of characters in the current line
-    char c;  // Variable to hold each character read from the file
+    size_t total_lines = 0;  // Counter for the total number of lines read.
+    size_t total_chars = 0;  // Counter for the number of characters in the current line.
+    char c;  // Variable to hold each character read from the file.
 
-    // Read the file character by character until the end of the file is reached
+    // Read the file character by character until the end of the file is reached.
     while ((c = fgetc(fptr)) != EOF) {
-        // If this is the first character in a line, allocate memory for the line
+        // If this is the first character in a line, allocate memory for the line.
         if (total_chars == 0) {
             lines[total_lines] = malloc(MORE_CHARS);
         }
 
-        // Store the character in the current line
+        // Store the character in the current line.
         lines[total_lines][total_chars] = c;
-        total_chars++;  // Increment the character count for the current line
+        total_chars++;  // Increment the character count for the current line.
 
-        // Check if the current character is a newline character
+        // Check if the current character is a newline character.
         if (c == '\n') {
-            lines[total_lines][total_chars] = '\0';  // Null-terminate the current line
-            total_lines++;  // Increment the line count
-            total_chars = 0;  // Reset the character count for the next line
+            lines[total_lines][total_chars] = '\0';  // Null-terminate the current line.
+            total_lines++;  // Increment the line count.
+            total_chars = 0;  // Reset the character count for the next line.
 
-            // If the number of lines exceeds the current allocated memory, reallocate more space
+            // If the number of lines exceeds the current allocated memory, reallocate more space.
             if (total_lines % MORE_LINES == 0) {
                 lines = realloc(lines, sizeof(char *) * (total_lines + MORE_LINES));
             }
         } 
-        // If the line exceeds the current allocated memory for characters, reallocate more space
+        // If the line exceeds the current allocated memory for characters, reallocate more space.
         else if (total_chars % MORE_CHARS == 0) {
             lines[total_lines] = realloc(lines[total_lines], total_chars + MORE_CHARS);
         }
     }
 
-    // Close the file since we've finished reading
+    // Close the file since we've finished reading.
     fclose(fptr);
 
-    // Null-terminate the array of lines so we know where the end of the array is
+    // Null-terminate the array of lines so we know where the end of the array is.
     lines = realloc(lines, sizeof(char *) * (total_lines + 1));
-    lines[total_lines] = NULL;  // Set the last element to NULL
+    lines[total_lines] = NULL;  // Set the last element to NULL.
 
-    return lines;  // Return the array of lines to the caller
+    return lines;  // Return the array of lines to the caller.
 }
