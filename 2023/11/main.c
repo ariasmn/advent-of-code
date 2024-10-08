491e4ed4e7dfd6ad0518762c99a676a7c12e139a
Ismael Arias <ismaelariasmn@gmail.com>
2024-10-08 21:00:16 +0200

    Finish day 11 part 2

--- 2023/11/main.c
+++ 2023/11/main.c
@@ -8,11 +8,11 @@
 
 struct Galaxy
 {
-    int x;
-    int y;
+    long long int x;
+    long long int y;
 };
 
-void expand_galaxies(struct Galaxy galaxies[], int size)
+void expand_galaxies(struct Galaxy galaxies[], int size, long long int times_to_expand)
 {
     // Find max x and y
     int max_x = 0, max_y = 0;
@@ -28,7 +28,7 @@ void expand_galaxies(struct Galaxy galaxies[], int size)
     int *shift_x = calloc(max_x + 1, sizeof(int));
     int *shift_y = calloc(max_y + 1, sizeof(int));
 
-    // Mark present values
+    // Add where the galaxies are in x/y.
     for (int i = 0; i < size; i++)
     {
         shift_x[galaxies[i].x] = 1;
@@ -36,12 +36,14 @@ void expand_galaxies(struct Galaxy galaxies[], int size)
     }
 
     // Calculate cumulative shifts
-    int count_x = 0, count_ = 0;
+    // Whenever is empty, we increase the count and assign to the shitf arrays,
+    // which will be used to update the galaxies later on.
+    int count_x = 0, count_y = 0;
     for (int i = 0; i <= max_x; i++)
     {
         if (shift_x[i] == 0)
         {
-            count_x++;
+            count_x += times_to_expand;
         }
         shift_x[i] = count_x;
     }
@@ -49,9 +51,9 @@ void expand_galaxies(struct Galaxy galaxies[], int size)
     {
         if (shift_y[i] == 0)
         {
-            count_++;
+            count_y += times_to_expand;
         }
-        shift_y[i] = count_;
+        shift_y[i] = count_y;
     }
 
     // Update coordinates
@@ -86,7 +88,7 @@ int part_1(char **file_content)
     }
 
     // We expand the galaxies, meaning, incrementing the x/y values when needed.
-    expand_galaxies(galaxies, galaxies_arr_size);
+    expand_galaxies(galaxies, galaxies_arr_size, 1);
 
     // Now, calculate the distance between pairs.
     int total_distance = 0;
@@ -96,6 +98,45 @@ int part_1(char **file_content)
         for (int j = i + 1; j < galaxies_arr_size; j++)
         {
             int distance = abs(galaxies[i].x - galaxies[j].x) + abs(galaxies[i].y - galaxies[j].y);
+            total_distance += distance;
+        }
+    }
+
+    free(galaxies);
+    return total_distance;
+}
+
+long long int part_2(char **file_content)
+{
+    int galaxies_arr_size = 0;
+    struct Galaxy *galaxies = malloc(1 * sizeof(struct Galaxy));
+
+    for (int i = 0; file_content[i] != NULL; i++)
+    {
+        for (int j = 0; file_content[i][j] != '\0'; j++)
+        {
+            if (file_content[i][j] == '#')
+            {
+                struct Galaxy galaxy = {.x = j, .y = i};
+                galaxies = realloc(galaxies, (galaxies_arr_size + 1) * sizeof(struct Galaxy));
+                galaxies[galaxies_arr_size] = galaxy;
+                galaxies_arr_size++;
+            }
+        }
+    }
+
+    // Same as part 1, just changed the expand_galaxies function to pass it how many times it
+    // needs to expand.
+
+    // We expand the galaxies, meaning, incrementing the x/y values when needed.
+    expand_galaxies(galaxies, galaxies_arr_size, 999999);
+
+    long long int total_distance = 0;
+    for (int i = 0; i < galaxies_arr_size - 1; i++)
+    {
+        for (int j = i + 1; j < galaxies_arr_size; j++)
+        {
+            int distance = llabs(galaxies[i].x - galaxies[j].x) + llabs(galaxies[i].y - galaxies[j].y);
             total_distance += distance;
         }
     }
@@ -115,5 +156,5 @@ int main()
     }
 
     printf("Part 1: %d\n", part_1(file_content));
-    // printf("Part 2: %d\n", part_2(file_content));
+    printf("Part 2: %lld\n", part_2(file_content));
 }
